f175adcc9b57d3886d7be78516ec5dd124020a41
christianfm10 <christian.flores@dekoding.pe>
2023-08-17 18:44:20 -0500

    Intro to Algorithms Ex 2.2

--- utils/utils.js
+++ utils/utils.js
@@ -1,1 +1,11 @@
-
+//import chalk from "chalk"
+const iterative_method = () => {
+    let a = 0;
+    let b = 10;
+    for (let n = 10; a < b; n++) {
+        a = 2 ** (n/8);
+        b = n;
+        console.log(a, b, n);
+    }
+}
+iterative_method()
